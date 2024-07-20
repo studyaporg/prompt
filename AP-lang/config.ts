909ac96b827e7f9c77674988cdca70eddb7af88c
AWESOMEHACKER459 <chuastevenson2@gmail.com>
2024-07-20 00:54:43 -0700

    Update config.ts

--- AP-lang/config.ts
+++ AP-lang/config.ts
@@ -4,33 +4,31 @@ export const config: Config = {
   version: 1,
   subjectName: "AP Language and Composition",
   unitNames: [
-    "Unit 1: Rhetorical Situation: Reading",
-    "Unit 2: Rhetorical Situation: Writing",
-    "Unit 3: Claims and Evidence: Reasoning",
-    "Unit 4: Learning",
-    "Unit 5: Cognitive Psychology",
-    "Unit 6: Developmental Psychology",
-    "Unit 7: Motivation, Emotion, and Personality",
-    "Unit 8: Clinical Psychology",
-    "Unit 9: Social Psychology",
+    "Unit 1: Rhetorical Situation:Reading",
+    "Unit 2: Rhetorical Situation:Writing",
+    "Unit 3: Claims and Evidence:Reading",
+    "Unit 4: Claims and Evidence:Writing",
+    "Unit 5: Reasoning and Organization:Reading",
+    "Unit 6: Reasoning and Organization:Writing",
+    "Unit 7: Style:Reading",
+    "Unit 8: Style:Writing",
   ],
   stimuli: {
     systemText:
       "You are a high school psychology teacher employed by the collegeboard to create stimuli for questions on the AP Psychology exam.",
     descriptions: {
-      text: "The plain text content of the stimulus, this must be a hypothetical situation you create to test the application of an AP Psychology term. The text that you CREATE should be relatively short, at most 2 sentences. You should describe the events without directly stating the term and give enough detail to determine the right answer. You also have the option to describe a photograph or graph in this field, if you do, set the 'image' field to TRUE.",
+      text: "",
       attribution: "",
     },
-    stimuliPerUnit: 3,
+    stimuliPerUnit: 0,
     doubleUnitCount: 20,
     tripleUnitCount: 5,
   },
   mcqs: {
     systemText:
-      "You are a high school history teacher employed by the collegeboard to create multiple choice questions for the AP Psychology exam.",
+      "You are a high school English teacher employed by the collegeboard to create multiple choice questions for the AP Language and Composition exam.",
     descriptions: {
-      question:
-        "The plain text content of the multiple choice question. This should ask which AP Psychology term is most relevant to the given stimulus. DO NOT ask questions about which term is from which unit. The unit SHOULD NOT be included in the question and the question SHOULD NOT ask anything about which term from unit ___ fits best. DO NOT use escape characters in your prompt.",
+      question:The plain text content of the stimulus, should have reading passages that require a decent amount of comprehension to understand thoroughly. The questions should have students understand the text content's main ideas and method of writing. The questions should also have students understand the author's rhetorical strategies and connect it to words such as: pathos,ethos,logos. Finally, students should be required to understand a vocab's word definition based off the context of the text",
     },
     questionsPerStimulus: 3,
     doubleAnswerCount: 20,
@@ -42,6 +40,6 @@ export const config: Config = {
       question:
         "The plain text question content of the free response question. Start by saying: 'You have 50 minutes to answer BOTH of the following questions. It is not enough to answer a question by merely listing facts. You should present a cogent argument based on your critical analysis of the questions posed, using appropriate psychological terminology.' Then list SEVEN UNIQUE psychology terms and grade based on whether or not they (1) defined the term and (2) correctly applied it to this situation.",
     },
-    questionsPerStimulus: 4,
+    questionsPerStimulus: 0,
   },
 }
